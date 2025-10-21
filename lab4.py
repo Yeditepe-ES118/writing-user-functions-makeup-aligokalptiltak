69193c15e0e4cb7f61cb05aeb93fb8f7d18c40fa
aligokalptiltak <aligokalp.tiltak@std.yeditepe.edu.tr>
2025-10-21 13:19:03 +0300

    lab4 makeup

--- lab4.py
+++ lab4.py
@@ -13,7 +13,7 @@ def throw_rock(m, v_0, theta):
     print("For a rock with %5.3f kg mass thrown with %5.3f m/s at an angle of %6.2f degrees:\n" \
           "Time of flight is %10.1e s\n"\
           "The range in x-direction is %10.1e m\n"\
-          "The maximum height is %10.1e m\n"\
+          "Maximum height is %10.1e m\n"\
           "The speed at maximum height is %10.1e m/s\n"\
           "Kinetic energy at the maximum height is %8.2e J" % (m, v_0, theta*180/np.pi, t_f, R, h_m, v_h, K_h))
     
